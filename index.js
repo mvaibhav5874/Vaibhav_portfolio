f166f61f870bfa8d404538acc3f67a0584391ead
mvaibhav5874 <mvaibhav5874@gmail.com>
2024-12-10 16:00:28 +0530

    commitedit

---
+++ index.js
@@ -0,0 +1,5 @@
+function OpenCertification(cert_number) {
+    if (cert_number === 0){
+        window.open("https://www.coursera.org/account/accomplishments/specialization/certificate/WGV62P8F83SW");
+    }
+}
