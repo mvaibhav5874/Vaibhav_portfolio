9c8183c1bc41ef4a7de999fcadb1a79456c9ec12
mvaibhav5874 <mvaibhav5874@gmail.com>
2024-12-11 10:58:20 +0530

    11dec

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 function OpenCertification(cert_number) {
-    if (cert_number === 0){
-        window.open("https://www.coursera.org/account/accomplishments/specialization/certificate/WGV62P8F83SW");
+    switch(cert_number){
+        case 0:
+        window.open("https://coursera.org/share/bb7e9ff90bf71e2fbc709c88363c9d27");   
+        break;
+        case 1:
+            window.open("https://www.credly.com/badges/ed824fb7-0361-4fcb-86f6-ef681c7b6bbf/linked_in?t=saxx0g");
+        break;
+        case 2:
+            window.open("https://drive.google.com/file/d/1tegNhDWVWjr1V3a8nh99QSZ8JYTQ7W1i/view?usp=sharing");
+        break;
+        case 3:
+            window.open("https://www.cloudskillsboost.google/public_profiles/94b6f9b3-b8a4-4e7e-98b7-2d988aad1d63/badges/9835184");
+        break;
+        case 4:
+            window.open("https://coursera.org/share/b21a3cb6199100f66cc479c38150fa68");
+        break;
+        case 5:
+            window.open("https://drive.google.com/file/d/1TMYhiYAg45JGMRxIsugJNwX9hAGZf5Om/view?usp=sharing");
+        break;
     }
 }
